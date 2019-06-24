df16c2597ed1ee0a8a30179aed201c40ec260be0
Leonardo Vaquel <leonardovaquel@gmail.com>
2019-06-24 15:58:41 -0300

    Updated accounts and version

--- src/config/config.js
+++ src/config/config.js
@@ -7,11 +7,6 @@ export default
       "page_name": "https://www.facebook.com/AlferdezPRENSA/"
     },
     {
-      "name": "Marcos Peña",
-      "page_id": "313852760141",
-      "page_name": "https://www.facebook.com/pena.marcos/"
-    },
-    {
       "name": "Mauricio Macri",
       "page_id": "55432788477",
       "page_name": "https://www.facebook.com/mauriciomacri/"
@@ -22,97 +17,12 @@ export default
       "page_name": "https://www.facebook.com/CFKArgentina/"
     },
     {
-      "name": "Gabriela Michetti",
-      "page_id": "60881841463",
-      "page_name": "https://www.facebook.com/gabrielamichetti/"
-    },
-    {
-      "name": "Axel Kicillof",
-      "page_id": "116053261893854",
-      "page_name": "https://www.facebook.com/kicillofok/"
-    },
-    {
-      "name": "María Eugenia Vidal",
-      "page_id": "183940131655054",
-      "page_name": "https://www.facebook.com/meugeniavidal/"
-    },
-    {
-      "name": "Agustin Rossi",
-      "page_id": "183533924508",
-      "page_name": "https://www.facebook.com/RossiAgustin/"
-    },
-    {
-      "name": "Juan Manuel Urtubey",
-      "page_id": "219322974826890",
-      "page_name": "https://www.facebook.com/JMUrtubey/"
-    },
-    {
-      "name": "Felipe Solá",
-      "page_id": "189596797759279",
-      "page_name": "https://www.facebook.com/felipesola/"
-    },
-    {
-      "name": "Sergio Massa",
-      "page_id": "374082229927",
-      "page_name": "https://www.facebook.com/SergioMassaOK/"
-    },
-    {
-      "name": "Margarita Stolbizer",
-      "page_id": "66774510472",
-      "page_name": "https://www.facebook.com/Stolbizer/"
-    },
-    {
-      "name": "Patricia Bullrich",
-      "page_id": "90148244993",
-      "page_name": "https://www.facebook.com/bullrichpatricia/"
-    },
-    {
-      "name": "Daniel Scioli",
-      "page_id": "39940248786",
-      "page_name": "https://www.facebook.com/danielsciolioficial/"
-    },
-    {
-      "name": "Martín Insaurralde",
-      "page_id": "174876119204869",
-      "page_name": "https://www.facebook.com/insaurraldem/"
-    },
-    {
-      "name": "Elisa Lilita Carrió",
-      "page_id": "70666562597",
-      "page_name": "https://www.facebook.com/Elisa.Lilita.Carrio/"
-    },
-    {
-      "name": "Martín Lousteau",
-      "page_id": "525334380868858",
-      "page_name": "https://www.facebook.com/GugaLusto/"
-    },
-    {
-      "name": "Juan Schiaretti",
-      "page_id": "565339116837552",
-      "page_name": "https://www.facebook.com/SchiarettiOk/"
-    },
-    {
-      "name": "Gerardo Morales",
-      "page_id": "61279025897",
-      "page_name": "https://www.facebook.com/GobernadorGerardoMorales/"
-    },
-    {
-      "name": "Cambiemos",
-      "page_id": "543211639151507",
-      "page_name": "https://www.facebook.com/cambiemos/"
-    },
-    {
-      "name": "Unidad Ciudadana",
-      "page_id": "141702659724594",
-      "page_name": "https://www.facebook.com/UnidadCiudadanaAR/"
-    },
-    {
       "name": "Unión Cívica Radical",
       "page_id": "346085120863",
       "page_name": "https://www.facebook.com/ucrnacional/"
     },
     {
-      "name": "PRO",
+      "name": "Propuesta Republicana (PRO)",
       "page_id": "79179986996",
       "page_name": "https://www.facebook.com/ProArgentina/"
     },
@@ -142,11 +52,6 @@ export default
       "page_name": "https://www.facebook.com/MinInteriorAR/"
     },
     {
-      "name": "Ministerio de Defensa - Argentina",
-      "page_id": "306610682812363",
-      "page_name": "https://www.facebook.com/MindefArg/"
-    },
-    {
       "name": "ANSES",
       "page_id": "127591817259266",
       "page_name": "https://www.facebook.com/ansesgob/"
@@ -155,11 +60,6 @@ export default
       "name": "AFIP Comunica",
       "page_id": "220961594979688",
       "page_name": "https://www.facebook.com/AFIPComunica/"
-    },
-    {
-      "name": "Ministerio de Producción y Trabajo",
-      "page_id": "140324806092935",
-      "page_name": "https://www.facebook.com/ProdArgentina/"
     },
     {
       "name": "Ministerio de Salud y Desarrollo Social de la Nación",
@@ -182,24 +82,164 @@ export default
       "page_name": "https://www.facebook.com/MAPichetto/"
     },
     {
-      "name": "Alfredo Cornejo",
-      "page_id": "714605368619727",
-      "page_name": "https://www.facebook.com/CornejoAlfredo/"
+      "name": "Ministerio de Educación, Cultura, Ciencia y Tecnología",
+      "page_id": "1717735835107639",
+      "page_name": "https://www.facebook.com/educacionAR/"
     },
     {
-      "name": "José Luis Gioja",
-      "page_id": "398675146900419",
-      "page_name": "https://www.facebook.com/joseluisgioja/"
+      "name": "Agencia de Deportes Nacional",
+      "page_id": "1116073295099975",
+      "page_name": "https://www.facebook.com/AgenciadeDeporteNacional/"
     },
     {
-      "name": "Verónica Magario",
-      "page_id": "195517260610729",
-      "page_name": "https://www.facebook.com/magariovero/"
+      "name": "Ministerio de Transporte",
+      "page_id": "905409642879079",
+      "page_name": "https://www.facebook.com/MinisterioDeTransporteDeLaNacionArgentina/"
     },
     {
-      "name": "Fernando Espinoza",
-      "page_id": "147255748694051",
-      "page_name": "https://www.facebook.com/FerEspinozaOK/"
+      "name": "Instituto Nacional de la Juventud",
+      "page_id": "1745381562369344",
+      "page_name": "https://www.facebook.com/injuvearg/"
+    },
+    {
+      "name": "Hablemos de todo",
+      "page_id": "398723283818370",
+      "page_name": "https://www.facebook.com/hablemosdetodok/"
+    },
+    {
+      "name": "Secretaría de Cultura de la Nación",
+      "page_id": "1095705940451512",
+      "page_name": "https://www.facebook.com/CulturaNacionAr/"
+    },
+    {
+      "name": "BA Emprende",
+      "page_id": "115421361814981",
+      "page_name": "https://www.facebook.com/BAemprende/"
+    },
+    {
+      "name": "Tecnópolis Argentina",
+      "page_id": "123982561016447",
+      "page_name": "https://www.facebook.com/tecnopolisargentina/"
+    },
+    {
+      "name": "BA Joven",
+      "page_id": "368884766483609",
+      "page_name": "https://www.facebook.com/BaJoven/"
+    },
+    {
+      "name": "Gobierno de la CABA",
+      "page_id": "113686738612",
+      "page_name": "https://www.facebook.com/GCBA/"
+    },
+    {
+      "name": "BA Partcipación",
+      "page_id": "487135334739165",
+      "page_name": "https://www.facebook.com/baparticipacion/"
+    },
+    {
+      "name": "Disfrutemos Buenos Aires",
+      "page_id": "172674689422072",
+      "page_name": "https://www.facebook.com/DisfrutemosBA/"
+    },
+    {
+      "name": "Balcarce, el perrito del PRO",
+      "page_id": "403224699865852",
+      "page_name": "https://www.facebook.com/ElPerritoDelPRO/"
+    },
+    {
+      "name": "Juntos Por el Cambio",
+      "page_id": "543211639151507",
+      "page_name": "https://www.facebook.com/juntosporelcambio/"
+    },
+    {
+      "name": "Partido Fé",
+      "page_id": "417286484998083",
+      "page_name": "https://www.facebook.com/PartidoFe/"
+    },
+    {
+      "name": "Fundación Pensar",
+      "page_id": "126410234048229",
+      "page_name": "https://www.facebook.com/fundacionpensar.org/"
+    },
+    {
+      "name": "Sociedad Rural",
+      "page_id": "185062668205813",
+      "page_name": "https://www.facebook.com/sociedadruralargentina/"
+    },
+    {
+      "name": "Coalición Cívica",
+      "page_id": "358664934192390",
+      "page_name": "https://www.facebook.com/CoalicionCivicaAriNacional/"
+    },
+    {
+      "name": "Instituto Patria",
+      "page_id": "1515515852077822",
+      "page_name": "https://www.facebook.com/InstitutoPATRIAar/"
+    },
+    {
+      "name": "Frente de Todos",
+      "page_id": "364916637463104",
+      "page_name": "https://www.facebook.com/frentedetodos/"
+    },
+    {
+      "name": "Frente Renovador",
+      "page_id": "122893371254786",
+      "page_name": "https://www.facebook.com/frenterenovador/"
+    },
+    {
+      "name": "Kolina",
+      "page_id": "439643502748736",
+      "page_name": "https://www.facebook.com/kolina.nacional/"
+    },
+    {
+      "name": "Partido Intransigente",
+      "page_id": "119863528030968",
+      "page_name": "https://www.facebook.com/Partido-Intransigente-119863528030968/"
+    },
+    {
+      "name": "Partido Comunista",
+      "page_id": "613930958656494",
+      "page_name": "https://www.facebook.com/pcaarg/"
+    },
+    {
+      "name": "Unidad Popular",
+      "page_id": "256836781181724",
+      "page_name": "https://www.facebook.com/unidadpopularnacional/"
+    },
+    {
+      "name": "Partido Solidario",
+      "page_id": "187191884963194",
+      "page_name": "https://www.facebook.com/PSOLArgentina/"
+    },
+    {
+      "name": "Partido del Trabajo y el Pueblo",
+      "page_id": "697544800374022",
+      "page_name": "https://www.facebook.com/PTPNACIONAL/"
+    },
+    {
+      "name": "Frente Grande",
+      "page_id": "1180984595261522",
+      "page_name": "https://www.facebook.com/Frente-Grande--1180984595261522/"
+    },
+    {
+      "name": "Proyecto Sur",
+      "page_id": "1246444838781826",
+      "page_name": "https://www.facebook.com/ProyectoSurNac/"
+    },
+    {
+      "name": "Partido Justicialista",
+      "page_id": "366171313436990",
+      "page_name": "https://www.facebook.com/pjusticialista/"
+    },
+    {
+      "name": "Nuevo Encuentro",
+      "page_id": "1392808114277429",
+      "page_name": "https://www.facebook.com/NuevoEncuentro.nacional/"
+    },
+    {
+      "name": "Diputados FPV PJ",
+      "page_id": "1434758056783232",
+      "page_name": "https://www.facebook.com/DiputadosFPVPJ/"
     }
   ],
   "locations": [
