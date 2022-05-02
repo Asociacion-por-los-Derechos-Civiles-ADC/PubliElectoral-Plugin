4803e88ef51cb9af66eb7c318e7952005b533441
Leonardo Vaquel <leonardo.vaquel@camba.coop>
2022-05-02 16:52:36 -0300

    Se configuran textos de traducciones segun locale del navegador

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,8 +1,26 @@
 import ext from "./utils/ext";
 import storage from "./utils/storage";
-import config from "../config/config.js"
+import config from "../config/config.js";
+import * as ptmessages from '../_locales/pt/messages.json';
+import * as esmessages from '../_locales/es/messages.json';
+import * as enmessages from '../_locales/en/messages.json';
 
-const locations = config.locations
+const locale = navigator.language
+let locations = []
+let tranlations = {}
+
+if (locale.includes('pt')) {
+  tranlations = ptmessages
+  locations = config.locations.pt
+}
+if (locale.includes('es')) {
+  tranlations = esmessages
+  locations = config.locations.es
+}
+if (locale.includes('en')) {
+  tranlations = enmessages
+  locations = config.locations.en
+}
 
 document.addEventListener("DOMContentLoaded", function(event) {
   var locationSelect = document.getElementById("location")
@@ -24,4 +42,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
   locationSelect.addEventListener("change", function(e) {
     storage.set({pub_elec_location: e.target.value})
   })
+
+  let appDescription = document.getElementById('appDescription_message')
+  appDescription.innerHTML = tranlations.appDescription.message
+
+  let appAbout = document.getElementById('about')
+  appAbout.innerHTML = tranlations.appDescription.about
+
+  let appSelectCountry = document.getElementById('selectCountry')
+  appSelectCountry.innerHTML = tranlations.selectCountry.message
 })
